0579d2a940d796efcaeea2a1f5ba8368d459ad77
Caio <cs1919328@gmail.com>
2024-05-01 22:23:35 -0300

    criando um game de yogioh

--- yogioh/src/Deck.java
+++ yogioh/src/Deck.java
@@ -58,7 +58,7 @@ public class Deck {
   }
 
   public void iniciarGame() {
-    for(int i = 0; i<2;i++) {
+    for(int i = 0; i<3;i++) {
       int auxi = randomNum(0, indice-1);
       mao[i] = deck[auxi];
       deck[auxi] = deck[indice-1];
@@ -66,7 +66,7 @@ public class Deck {
       indice--;
     }
 
-    for(int i = 0; i<2; i++) {
+    for(int i = 0; i<3; i++) {
       mao[i].imprimir();
       System.out.println("----------------------------------X");
     }
